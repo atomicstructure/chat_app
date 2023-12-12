b3a766ba3c77d1f68f73cf44b0dd91702fba4548
Ogundare Olusesi Oluwafemi <ogundarearold@gmail.com>
2023-12-12 12:55:45 +0100

    Loaded and displayed chat messages as a stream

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget{
@@ -5,6 +6,31 @@ class ChatMessages extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('No messages found yet'),);
-  }
+
+    return StreamBuilder(stream: FirebaseFirestore.instance
+    .collection('chat')
+    .orderBy(
+      'createdAt', 
+      descending: false
+    )
+    .snapshots(), 
+    builder: (context, chatSnapshot) {
+      if (chatSnapshot.connectionState == ConnectionState.waiting) {
+        return const Center(child: CircularProgressIndicator(),
+        );
+      }
+      if (!chatSnapshot.hasData || chatSnapshot.data!.docs.isEmpty) {
+        return const Center(child: Text('No messages found yet'),);
+      }
+      if (chatSnapshot.hasError) {
+        return const Center(child: Text('Oops...Something went wrong!'),);
+      }
+
+      final loadedMessages = chatSnapshot.data!.docs;
+      return ListView.builder(itemBuilder: (context, index) {
+        return Text(loadedMessages[index].data()['text']);
+      }, itemCount: loadedMessages.length,);
+    }
+  );
+}
 }
